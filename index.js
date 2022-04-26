0da1f5a6edad3c2e1508b69e592570f7d38d1d7a
jad-la <Eljadla.Ennih@outlook.fr>
2022-04-26 14:56:17 +0200

    Jadla suite partie océane en css

---
+++ index.js
@@ -0,0 +1,6 @@
+let menuBurger = document.querySelector('.menuBurger');
+let body = document.querySelector('body');
+
+menuBurger.addEventListener('click', function(){
+  body.classList.menuBurger('open');
+})
